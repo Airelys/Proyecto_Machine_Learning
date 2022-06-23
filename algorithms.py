b906d80f4858d2127414b5671c223cda1941c1a7
AleEscGir <ale.kiritov@gmail.com>
2022-06-23 00:40:41 -0400

    Actualizado el main y organizadas algunas cosas

--- algorithms.py
+++ algorithms.py
@@ -13,107 +13,108 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import plot_confusion_matrix
 
-
 import warnings
 
 warnings.filterwarnings("ignore")
 
-n_samples = 300
-outliers_fraction = 0.15
-n_outliers = int(outliers_fraction*n_samples)
-n_inliers = n_samples - n_outliers
+def run_algorithms():
 
-non_supervised_algorithms = [
-    ("One Class SVM", svm.OneClassSVM(nu=outliers_fraction, kernel='rbf', gamma=0.1)),
-    ("Robust covariance", EllipticEnvelope(contamination=outliers_fraction)),
-    ("Isolation Forest", IsolationForest(contamination=outliers_fraction, random_state=42))
-]
+    n_samples = 300
+    outliers_fraction = 0.15
+    n_outliers = int(outliers_fraction*n_samples)
+    n_inliers = n_samples - n_outliers
 
-supervised_algorithms = [("GaussianNB", GaussianNB()),
-                         ("KNeighborsClassifier", KNeighborsClassifier()),
-                         ("DecisionTreeClassifier", DecisionTreeClassifier(criterion = "entropy")),
-                         ("RandomForestClassifier", RandomForestClassifier(criterion = "entropy"))]
+    non_supervised_algorithms = [
+        ("One Class SVM", svm.OneClassSVM(nu=outliers_fraction, kernel='rbf', gamma=0.1)),
+        ("Robust covariance", EllipticEnvelope(contamination=outliers_fraction)),
+        ("Isolation Forest", IsolationForest(contamination=outliers_fraction, random_state=42))
+    ]
 
-names, dataset = reduction()
+    supervised_algorithms = [("GaussianNB", GaussianNB()),
+                            ("KNeighborsClassifier", KNeighborsClassifier()),
+                            ("DecisionTreeClassifier", DecisionTreeClassifier(criterion = "entropy")),
+                            ("RandomForestClassifier", RandomForestClassifier(criterion = "entropy"))]
 
-print(names)
+    names, dataset = reduction()
 
-
-
-rng = np.random.RandomState(42)
+    print(names)
 
 
-plt.figure(figsize=(8,6))
-plt.subplots_adjust(left=.02, right=.98, bottom=.001, top=.96, wspace=.05,
-                    hspace=.01)
-plot_num = 1
-count = 0 
+
+    rng = np.random.RandomState(42)
 
 
-# Puesta en marcha de los algoritmos de aprendizaje no supervisado:
+    plt.figure(figsize=(8,6))
+    plt.subplots_adjust(left=.02, right=.98, bottom=.001, top=.96, wspace=.05,
+                        hspace=.01)
+    plot_num = 1
+    count = 0 
 
 
-for X in dataset:
-    for name,algorithm in non_supervised_algorithms:
-        print(name + '\n')
-        t0 = time.time()
-        algorithm.fit(X)
-        t1 = time.time()
-        plt.subplot(len(dataset), len(non_supervised_algorithms), plot_num)
-        if count == 0:
-            plt.title(name, size=18)
-
-        y_predict = algorithm.fit(X).predict(X)
-        print("y_predict \n" + str(y_predict))
-
-        colors = np.array(['#377eb8', '#ff7f00'])
-        plt.scatter(X[:, 0], X[:, 1], s=10, color=colors[(y_predict+1)//2 ])
-
-        plt.xlim(-2, 2)
-        plt.ylim(-2, 2)
-        plt.xticks(())
-        plt.yticks(())
-        plt.text(.99, .01, ('%.2fs' % (t1 - t0)).lstrip('0'),
-                 transform=plt.gca().transAxes, size=15,
-                 horizontalalignment='right')
-        plot_num += 1
-
-    count += 1
-
-plt.show()
+    # Puesta en marcha de los algoritmos de aprendizaje no supervisado:
 
 
-# Puesta en marcha de los algoritmos de aprendizaje supervisado
+    for X in dataset:
+        for name,algorithm in non_supervised_algorithms:
+            print(name + '\n')
+            t0 = time.time()
+            algorithm.fit(X)
+            t1 = time.time()
+            plt.subplot(len(dataset), len(non_supervised_algorithms), plot_num)
+            if count == 0:
+                plt.title(name, size=18)
 
-y_for_evaluated = []
+            y_predict = algorithm.fit(X).predict(X)
+            print("y_predict \n" + str(y_predict))
 
-for i in range(len(dataset)):
-    y_for_evaluated.append([i] * len(dataset[i]))
+            colors = np.array(['#377eb8', '#ff7f00'])
+            plt.scatter(X[:, 0], X[:, 1], s=10, color=colors[(y_predict+1)//2 ])
 
-X_for_supervised = []
-y_for_supervised = []
+            plt.xlim(-2, 2)
+            plt.ylim(-2, 2)
+            plt.xticks(())
+            plt.yticks(())
+            plt.text(.99, .01, ('%.2fs' % (t1 - t0)).lstrip('0'),
+                    transform=plt.gca().transAxes, size=15,
+                    horizontalalignment='right')
+            plot_num += 1
 
-for i in range(len(dataset)):
-    for j in range(len(dataset[i])):
-        X_for_supervised.append(dataset[i][j])
-    y_for_supervised = y_for_supervised + y_for_evaluated[i]
+        count += 1
+
+    plt.show()
 
 
-X_train, X_test, y_train, y_test = train_test_split(X_for_supervised, y_for_supervised, train_size = 0.7)
+    # Puesta en marcha de los algoritmos de aprendizaje supervisado
+
+    y_for_evaluated = []
+
+    for i in range(len(dataset)):
+        y_for_evaluated.append([i] * len(dataset[i]))
+
+    X_for_supervised = []
+    y_for_supervised = []
+
+    for i in range(len(dataset)):
+        for j in range(len(dataset[i])):
+            X_for_supervised.append(dataset[i][j])
+        y_for_supervised = y_for_supervised + y_for_evaluated[i]
 
 
-for name, algorithm in supervised_algorithms:
-    
-    algorithm.fit(X_train, y_train)
-    
-    if len(names) < 6:
-        disp = plot_confusion_matrix(algorithm, X_test, y_test,
-                                display_labels=names,
-                                cmap=plt.cm.Blues,
-                                normalize='true')
-        disp.ax_.set_title("Algorithm Used: " + name + "\n" + "General_Score: " + str(algorithm.score(X_test, y_test)))
+    X_train, X_test, y_train, y_test = train_test_split(X_for_supervised, y_for_supervised, train_size = 0.7)
+
+
+    for name, algorithm in supervised_algorithms:
         
-        plt.show()
-    else:
-        print(name + " has score: " + str(algorithm.score(X_test, y_test)))
+        algorithm.fit(X_train, y_train)
+        
+        if len(names) < 6:
+            disp = plot_confusion_matrix(algorithm, X_test, y_test,
+                                    display_labels=names,
+                                    cmap=plt.cm.Blues,
+                                    normalize='true')
+            disp.ax_.set_title("Algorithm Used: " + name + "\n" + "General_Score: " + str(algorithm.score(X_test, y_test)))
+            
+            plt.show()
+        else:
+            print(name + " has score: " + str(algorithm.score(X_test, y_test)))
 
